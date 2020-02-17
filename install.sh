0b36326fb0002e5cf76fbacb72d24451b506eca1
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-02-17 10:53:47 +0000

    Relative path

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # link common dotfiles
-ln -s vimrc ~/.vimrc
-ln -s bash_profile ~/.bash_profile
-ln -s bash_aliases_personal ~/.bash_aliases_personal
-ln -s bash_aliases_lxplus ~/.bash_aliases_lxplus
-ln -s bash_aliases_mac ~/.bash_aliases_mac
-ln -s gitconfig_common ~/.gitconfig_common
-ln -s gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles_harmonised/vimrc ~/.vimrc
+ln -s ~/dotfiles_harmonised/bash_profile ~/.bash_profile
+ln -s ~/dotfiles_harmonised/bash_aliases_personal ~/.bash_aliases_personal
+ln -s ~/dotfiles_harmonised/bash_aliases_lxplus ~/.bash_aliases_lxplus
+ln -s ~/dotfiles_harmonised/bash_aliases_mac ~/.bash_aliases_mac
+ln -s ~/dotfiles_harmonised/gitconfig_common ~/.gitconfig_common
+ln -s ~/dotfiles_harmonised/gitignore_global ~/.gitignore_global
 
 # platform specific git config
 # CERN config for lxplus
 if [[ "$(hostname)" =~ "lxplus" ]]; then
-	ln -s gitconfig_lxplus ~/.gitconfig
+	ln -s ~/dotfiles_harmonised/gitconfig_lxplus ~/.gitconfig
 
 # personal otherwise
 elif [ "$(uname)" == "Darwin" ]; then
-	ln -s gitconfig_personal ~/.gitconfig
+	ln -s ~/dotfiles_harmonised/gitconfig_personal ~/.gitconfig
 fi
