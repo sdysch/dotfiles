d48fee9390807280f8c63ae501f727e3ee6a0df6
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-03-07 13:04:15 +0000

    tmux conf file

--- install_scripts/install_common.sh
+++ install_scripts/install_common.sh
@@ -6,3 +6,4 @@ ln -fsn ~/dotfiles_harmonised/vim/vimrc ~/.vimrc
 ln -fsn ~/dotfiles_harmonised/bash/bash_profile ~/.bash_profile
 ln -fsn ~/dotfiles_harmonised/git/gitignore_global ~/.gitignore_global
 ln -fsn ~/dotfiles_harmonised/zsh/zsh_aliases_common ~/.zsh_aliases_common
+ln -fsn ~/dotfiles_harmonised/tmux/tmux.conf ~/.tmux.conf
