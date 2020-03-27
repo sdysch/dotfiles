5820a498a9eac1d2fa8e904d1150934852b5a490
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-03-27 11:00:13 +0000

    Remove bash aliases

--- install_scripts/install_sam.sh
+++ install_scripts/install_sam.sh
@@ -6,8 +6,8 @@ source ~/dotfiles_harmonised/install_scripts/install_common.sh
 # install dotfiles for me
 
 ln -fsn ~/dotfiles_harmonised/git/gitconfig_personal ~/.gitconfig
-ln -fsn ~/dotfiles_harmonised/bash/bashrc ~/.bashrc
-ln -fsn ~/dotfiles_harmonised/bash/bash_aliases_sam ~/.bash_aliases_sam
+#ln -fsn ~/dotfiles_harmonised/bash/bashrc ~/.bashrc
+#ln -fsn ~/dotfiles_harmonised/bash/bash_aliases_sam ~/.bash_aliases_sam
 ln -fsn ~/dotfiles_harmonised/zsh/zshrc_sam ~/.zshrc
 ln -fsn ~/dotfiles_harmonised/zsh/zsh_aliases_personal ~/.zsh_aliases_personal
 ln -fsn ~/dotfiles_harmonised/tmux/tmux.conf ~/.tmux.conf
