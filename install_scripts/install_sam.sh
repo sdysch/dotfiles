55ffb6f34440e82baa7720e919830398a66cd506
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-03-29 12:42:11 +0100

    Link scripts into local bin directory

--- install_scripts/install_sam.sh
+++ install_scripts/install_sam.sh
@@ -16,9 +16,22 @@ ln -fsn ~/dotfiles_harmonised/tmux/tmux.conf ~/.tmux.conf
 mkdir -p ~/.i3
 ln -fsn ~/dotfiles_harmonised/i3/config ~/.config/i3/config
 #ln -fsn ~/dotfiles_harmonised/i3/i3status.conf ~/.config/i3/i3status.conf
-ln -fsn ~/dotfiles_harmonised/i3blocks.config ~/.config/i3blocks/config
+ln -fsn ~/dotfiles_harmonised/i3blocks/i3blocks.config ~/.config/i3blocks/config
 
 # nitrogen config
 mkdir -p ~/.config/nitrogen
 ln -fsn ~/dotfiles_harmonised/nitrogen/nitrogen.cfg ~/.config/nitrogen/nitrogen.cfg
 ln -fsn ~/dotfiles_harmonised/nitrogen/bg-saved.cfg ~/.config/nitrogen/bg-saved.cfg
+
+# link custom scripts
+declare -a scripts=("shutdown_script" "reboot" "logout" "prompt" "poweroptions")
+for script in ${scripts[@]}; do
+	ln -fsn ~/dotfiles_harmonised/scripts/$script ~/.local/bin/$script
+done
+
+# link custom scripts
+declare -a i3blocksscripts=("battery" "bandwidth" "battery2" "bandwidth2" "memory" "cpu_usage" "wifi" "disk_usage" "keyindicator" "volume")
+for script in ${i3blocksscripts[@]}; do
+	ln -fsn ~/dotfiles_harmonised/i3blocks/scripts/$script ~/.local/bin/$script
+done
+
