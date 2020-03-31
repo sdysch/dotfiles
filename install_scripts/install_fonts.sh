2674ab457a9383397ece38b252ea76e012eeb863
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-03-31 15:21:29 +0100

    Return to correct directory

--- install_scripts/install_fonts.sh
+++ install_scripts/install_fonts.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "Installing powerline fonts..."
+pwd=$(pwd)
 cd /tmp
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
 cd ..
+cd $pwd
 rm -rf fonts
 echo "Done"
