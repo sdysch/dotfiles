9fd3a1404b2fabffc302474e349056ad32a986b3
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-03-31 21:33:50 +0100

    Removing more hardcoded paths

--- install_scripts/install_sam.sh
+++ install_scripts/install_sam.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# installation locations
 INSTALLDIR=$(pwd)
+SCRIPTINSTALLDIR="${HOME}/.local/bin"
+TERMINALINSTALLDIR="${HOME}/.config/xfce4/terminal"
+NITROGENINSTALLDIR="${HOME}/.config/nitrogen"
+I3INSTALLDIR="${HOME}/.config/i3"
+I3BLOCKSINSTALLDIR="${HOME}/.config/i3blocks"
 
 # actually install the various packages?
 if [[ "$1" == "install" ]]; then
@@ -13,36 +19,37 @@ source $INSTALLDIR/install_scripts/install_common.sh $INSTALLDIR
 
 # install dotfiles for me
 
-ln -fsn ~/dotfiles_harmonised/git/gitconfig_personal ~/.gitconfig
-#ln -fsn ~/dotfiles_harmonised/bash/bashrc ~/.bashrc
-#ln -fsn ~/dotfiles_harmonised/bash/bash_aliases_sam ~/.bash_aliases_sam
-ln -fsn ~/dotfiles_harmonised/zsh/zshrc_sam ~/.zshrc
-ln -fsn ~/dotfiles_harmonised/zsh/zsh_aliases_personal ~/.zsh_aliases_personal
-ln -fsn ~/dotfiles_harmonised/tmux/tmux.conf ~/.tmux.conf
+ln -fsn ${INSTALLDIR}/git/gitconfig_personal ~/.gitconfig
+ln -fsn ${INSTALLDIR}/zsh/zshrc_sam ~/.zshrc
+ln -fsn ${INSTALLDIR}/zsh/zsh_aliases_personal ~/.zsh_aliases_personal
+ln -fsn ${INSTALLDIR}/tmux/tmux.conf ~/.tmux.conf
 
 # install i3 config
-mkdir -p ~/.config/i3
-mkdir -p ~/.config/i3blocks
-ln -fsn ~/dotfiles_harmonised/i3/config ~/.config/i3/config
-#ln -fsn ~/dotfiles_harmonised/i3/i3status.conf ~/.config/i3/i3status.conf
-ln -fsn ~/dotfiles_harmonised/i3blocks/i3blocks.config ~/.config/i3blocks/config
+mkdir -p $I3INSTALLDIR
+mkdir -p $I3BLOCKSINSTALLDIR
+ln -fsn ${INSTALLDIR}/i3/config $I3INSTALLDIR/config
+ln -fsn ${INSTALLDIR}/i3blocks/i3blocks.config $I3BLOCKSINSTALLDIR/config
 
 # nitrogen config
-mkdir -p ~/.config/nitrogen
-ln -fsn ~/dotfiles_harmonised/nitrogen/nitrogen.cfg ~/.config/nitrogen/nitrogen.cfg
-ln -fsn ~/dotfiles_harmonised/nitrogen/bg-saved.cfg ~/.config/nitrogen/bg-saved.cfg
+mkdir -p $NITROGENINSTALLDIR
+ln -fsn ${INSTALLDIR}/nitrogen/nitrogen.cfg ~/.config/nitrogen/nitrogen.cfg
+ln -fsn ${INSTALLDIR}/nitrogen/bg-saved.cfg ~/.config/nitrogen/bg-saved.cfg
+
+# installation location for custom scripts
+mkdir -p $SCRIPINSTALLDIR
 
 # link custom scripts
 declare -a scripts=("shutdown_script" "reboot" "logout" "prompt" "poweroptions" "corona" "corona_formatted" "notes")
 for script in ${scripts[@]}; do
-	ln -fsn ~/dotfiles_harmonised/scripts/$script ~/.local/bin/$script
+	ln -fsn ${INSTALLDIR}/scripts/$script $SCRIPTINSTALLDIR/$script
 done
 
 # link custom scripts
 declare -a i3blocksscripts=("battery" "bandwidth" "battery2" "bandwidth2" "memory" "cpu_usage" "wifi" "disk_usage" "keyindicator" "volume")
 for script in ${i3blocksscripts[@]}; do
-	ln -fsn ~/dotfiles_harmonised/i3blocks/scripts/$script ~/.local/bin/$script
+	ln -fsn ${INSTALLDIR}/i3blocks/scripts/$script $SCRIPTINSTALLDIR/$script
 done
 
 # xfce4 config
-ln -fsn ~/dotfiles_harmonised/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
+mkdir -p $TERMINALINSTALLDIR
+ln -fsn ${INSTALLDIR}/xfce4/terminal/terminalrc $TERMINALINSTALLDIR/terminalrc
