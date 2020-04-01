b46171e6eefc294b90d1aa1cd886ace5497d36ff
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-04-01 22:24:01 +0100

    Move ssh aliases to ssh config file

--- install_scripts/install_common.sh
+++ install_scripts/install_common.sh
@@ -8,3 +8,6 @@ ln -fsn ${INSTALLDIR}/bash/bash_profile ~/.bash_profile
 ln -fsn ${INSTALLDIR}/git/gitignore_global ~/.gitignore_global
 ln -fsn ${INSTALLDIR}/zsh/zsh_aliases_common ~/.zsh_aliases_common
 ln -fsn ${INSTALLDIR}/tmux/tmux.common.conf ~/.tmux.common.conf
+
+mkdir -p ~/.ssh
+ln -fsn ${INSTALLDIR}/ssh/config ~/.ssh/config
