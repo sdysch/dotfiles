77bf0b2fa6e117c4f1ec08095f5955793b294f37
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-04-23 18:12:30 +0100

    p10k theme

--- install_scripts/packages/install_zsh.sh
+++ install_scripts/packages/install_zsh.sh
@@ -21,4 +21,5 @@ echo "Installing zsh packages..."
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "Done"
