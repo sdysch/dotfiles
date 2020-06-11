6d4460c0bf80210728226ff24b44d00faba78e1f
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-06-11 23:50:01 +0100

    spiral layout

--- home/.config/i3/scripts/alternating_layouts.py
+++ home/.config/i3/scripts/alternating_layouts.py
@@ -1,106 +1,68 @@
 #!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
 
-import i3
-import re
-import subprocess
-import getopt
+"""
+This script uses the i3ipc python module to switch the layout splith / splitv
+for the currently focused window, depending on its dimensions.
+It works on both sway and i3 window managers.
+Inspired by https://github.com/olemartinorg/i3-alternating-layout
+Copyright: 2019-2020 Piotr Miller & Contributors
+e-mail: nwg.piotr@gmail.com
+Project: https://github.com/nwg-piotr/autotiling
+License: GPL3
+Dependencies: python-i3ipc>=2.0.1 (i3ipc-python)
+"""
+import argparse
 import sys
-import os
+from functools import partial
+
+from i3ipc import Connection, Event
 
 
-def find_parent(window_id):
-    """
-        Find the parent of a given window id
-    """
-    root_window = i3.get_tree()
-    result = [None]
+def switch_splitting(i3, e, debug):
+    try:
+        con = i3.get_tree().find_focused()
+        if con:
+            if con.floating:                         # We're on i3: on sway it would be None
+                is_floating = '_on' in con.floating  # May be 'auto_on' or 'user_on'
+                is_full_screen = con.fullscreen_mode == 1
+            else:                                    # We are on sway
+                is_floating = con.type == 'floating_con'
+                is_full_screen = con.fullscreen_mode == 1
 
-    def finder(n, p=None):
-        if result[0] is not None:
-            return
-        for node in n:
-            if node['id'] == window_id:
-                result[0] = p
-                return
-            if len(node['nodes']):
-                finder(node['nodes'], node)
+            is_stacked = con.parent.layout == 'stacked'
+            is_tabbed = con.parent.layout == 'tabbed'
 
-    finder(root_window['nodes'])
-    return result[0]
+            # Let's exclude floating containers, stacked layouts, tabbed layouts and full screen mode
+            if not is_floating and not is_stacked and not is_tabbed and not is_full_screen:
+                new_layout = 'splitv' if con.rect.height > con.rect.width else 'splith'
+                if new_layout != con.parent.layout:
+                    result = i3.command(new_layout)
+                    if result[0].success and debug:
+                        print('Debug: Switched to {}'.format(new_layout), file=sys.stderr)
+                    elif debug:
+                        print('Error: Switch failed with err {}'.format(result[0].error), file=sys.stderr)
 
+        elif debug:
+            print('Debug: No focused container found', file=sys.stderr)
 
-def set_layout():
-    """
-        Set the layout/split for the currently
-        focused window to either vertical or
-        horizontal, depending on its width/height
-    """
-    current_win = i3.filter(nodes=[], focused=True)
-    for win in current_win:
-        parent = find_parent(win['id'])
-
-        if (parent and "rect" in parent
-                   and parent['layout'] != 'tabbed'
-                   and parent['layout'] != 'stacked'):
-            height = parent['rect']['height']
-            width = parent['rect']['width']
-
-            if height > width:
-                new_layout = 'vertical'
-            else:
-                new_layout = 'horizontal'
-
-            i3.split(new_layout)
-
-
-def print_help():
-    print("Usage: " + sys.argv[0] + " [-p path/to/pid.file]")
-    print("")
-    print("Options:")
-    print("    -p path/to/pid.file   Saves the PID for this program in the filename specified")
-    print("")
+    except Exception as e:
+        print('Error: {}'.format(e), file=sys.stderr)
 
 
 def main():
-    """
-        Main function - listen for window focus
-        changes and call set_layout when focus
-        changes
-    """
-    opt_list, args = getopt.getopt(sys.argv[1:], 'hp:')
-    pid_file = None
-    for opt in opt_list:
-        if opt[0] == "-h":
-            print_help()
-            sys.exit()
-        if opt[0] == "-p":
-            pid_file = opt[1]
-
-    if pid_file:
-        with open(pid_file, 'w') as f:
-            f.write(str(os.getpid()))
-
-
-    process = subprocess.Popen(
-        ['xprop', '-root', '-spy'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--debug',
+        action='store_true',
+        help='Print debug messages to stderr'
     )
-    regex = re.compile(b'^_NET_CLIENT_LIST_STACKING|^_NET_ACTIVE_WINDOW')
+    args = parser.parse_args()
+    handler = partial(switch_splitting, debug=args.debug)
+    i3 = Connection()
+    i3.on(Event.WINDOW_FOCUS, handler)
+    i3.main()
 
-    last_line = ""
-    while True:
-        line = process.stdout.readline()
-        if line == b'': #X is dead
-            break
-        if line == last_line:
-            continue
-        if regex.match(line):
-            set_layout()
-        last_line = line
-
-    process.kill()
-    sys.exit()
 
 if __name__ == "__main__":
     main()
