171171187af8ce25c1f069096fff77ce4afad125
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-06-18 23:04:29 +0100

    Updates to install scripts

--- install_scripts/install_packages.sh
+++ install_scripts/install_packages.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+mkdir -p "${XDG_CONFIG_HOME:-=$HOME/.config}"
+mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"
+mkdir -p "${XDG_CACHE_HOME:-$HOME/.cache}"
+
 sudo apt-get install $(cat packages.txt)
 pip3 install -r python_packages.txt
 
