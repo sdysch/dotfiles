823a85dde07a565651a349a67587a6e5d10e91bf
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-06-18 20:59:15 +0100

    Add AUR package handler and LaTeX

--- install_scripts/install_packages.sh
+++ install_scripts/install_packages.sh
@@ -2,15 +2,16 @@
 sudo apt-get install $(cat packages.txt)
 pip3 install -r python_packages.txt
 
-echo "cloning taskwarrior"
-mkdir -p $HOME/.task
-git clone https://github.com/sdysch/taskwarrior_backups.git $HOME/.task/
+#echo "cloning taskwarrior"
+#mkdir -p $HOME/.task
+#git clone https://github.com/sdysch/taskwarrior_backups.git $HOME/.task/
 
 # install crontab
 cat crontab/crontab | crontab -
 
-scripts=(install_vim.sh install_fonts.sh install_zsh.sh)
-for script in ${scripts[@]}; do
-	source $script
-done
+#scripts=(install_vim.sh install_fonts.sh install_zsh.sh)
+#for script in ${scripts[@]}; do
+	#source $script
+#done
+
 #source install_scripts/packages/install_tmux.sh
