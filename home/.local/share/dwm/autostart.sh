9fea9b2ae3b0e1b6b1e4c11eed4e4e0ddfa85e23
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-06-20 17:34:08 +0100

    Updates

--- home/.local/share/dwm/autostart.sh
+++ home/.local/share/dwm/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # dwm autostart programs
-nm-applet
-compton -b --config ~/.config/compton/compton.conf
-pulseaudio --start
-~/.local/bin/check_inactivity
-nitrogen --restore
-gnome-keyring-daemon --start
+nm-applet &
+compton -b --config ~/.config/compton/compton.conf &
+pulseaudio --start &
+~/.local/bin/check_inactivity &
+nitrogen --restore &
+#gnome-keyring-daemon --start &
