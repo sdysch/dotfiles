e1b501ce4cb42270f7895b5e7ec9b58d9a7a0221
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-06-20 17:40:38 +0100

    Adding updates for dwm scripts

---
+++ home/.local/share/dwm/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# dwm autostart programs (consider adding to customised xinitrc in the future?)
+compton -b --config ~/.config/compton/compton.conf &
+gnome-keyring-daemon --start &
+nitrogen --restore &
+nm-applet &
+pulseaudio --start &
+~/.local/bin/check_inactivity &
