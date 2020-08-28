aa89a234491490e59d10f08cdc4a4ceccb16224a
Samuel Dysch <samuel.dysch@cern.ch>
2020-08-28 22:53:09 +0200

    Tidy up

--- install_scripts/install_lxplus.sh
+++ install_scripts/install_lxplus.sh
@@ -6,19 +6,21 @@ INSTALLDIR=$(pwd)
 mkdir -p ~/.config/vim
 mkdir -p ~/.config/tmux
 mkdir -p ~/.ssh
+mkdir -p ~/.cache/zsh
 
 ln -fsn ${INSTALLDIR}/common/bash/bash_aliases_lxplus ~/.bash_aliases
 ln -fsn ${INSTALLDIR}/common/bash/bash_profile ~/.bash_profile
+
 ln -fsn ${INSTALLDIR}/common/git/gitconfig_lxplus ~/.gitconfig
 ln -fsn ${INSTALLDIR}/common/git/gitignore_global ~/.gitignore_global
+
 #ln -fsn ${INSTALLDIR}/common/tmux/tmux.common.conf ~/.config/tmux/common.conf
 #ln -fsn ${INSTALLDIR}/common/tmux/tmux.lxplus.conf ~/.config/tmux/tmux.conf
+
 ln -fsn ${INSTALLDIR}/common/vim/vimrc ~/.config/vim/vimrc
 
-# do not use zsh on lxplus - xTauFW is not compatible?
-ln -fsn ${INSTALLDIR}/common/zsh/zshrc_lxplus ~/.zshrc
-ln -fsn ${INSTALLDIR}/common/zsh/zsh_aliases_lxplus ~/.zsh_aliases_lxplus
+ln -fsn ${INSTALLDIR}/lxplus/zshrc ~/.zshrc
+ln -fsn ${INSTALLDIR}/lxplus/zsh_aliases ~/.zsh_aliases
 ln -fsn ${INSTALLDIR}/common/zsh/zsh_aliases_common ~/.zsh_aliases_common
 
-mkdir -p ~/.ssh
 ln -fsn ${INSTALLDIR}/config/ssh/config ~/.ssh/config
