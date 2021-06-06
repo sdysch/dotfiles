669056b099beb1cacc0a53189742036d622330ae
Samuel Dezso Dysch <sdysch94@gmail.com>
2021-06-06 15:56:55 +0100

    Space

--- install_scripts/packages/install_fonts.sh
+++ install_scripts/packages/install_fonts.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 echo "Installing powerline fonts..."
 cd /tmp
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
 	echo "Could not change to /tmp, something went wrong"
 	exit 1
 fi
@@ -10,7 +10,7 @@ fi
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
 	echo "Could not change to /tmp/fonts, something went wrong"
 	exit 1
 fi
@@ -19,7 +19,7 @@ fi
 
 cd ..
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
 	echo "Could not change to root dir, something went wrong"
 	exit 1
 fi
