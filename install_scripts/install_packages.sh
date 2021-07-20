72be896d1e2f42b5af439aecd9f37c5b2389a55d
Samuel Dezso Dysch <sdysch94@gmail.com>
2021-07-20 16:16:10 +0100

    Couple of fixes for suckless install. Fix bugs in actual install script

--- install_scripts/install_packages.sh
+++ install_scripts/install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p "${XDG_CONFIG_HOME:-=$HOME/.config}"
+mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}"
 mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"
 mkdir -p "${XDG_CACHE_HOME:-$HOME/.cache}"
 
