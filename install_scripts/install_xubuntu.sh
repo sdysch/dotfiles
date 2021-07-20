109f8d9f8633e7c44297ea355dc024718287d9c6
Samuel Dezso Dysch <sdysch94@gmail.com>
2021-07-20 19:18:38 +0100

    Tidy up install scripts to work better with docker

--- install_scripts/install_xubuntu.sh
+++ install_scripts/install_xubuntu.sh
@@ -5,6 +5,7 @@ mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"
 mkdir -p "${XDG_CACHE_HOME:-$HOME/.cache}"
 
 # don't want to install many packages for each CI run
+# they are also not needed for the docker container
 if [[ ! ${CI} ]]; then
 	sudo apt-get install $(cat packages/packages_xubuntu.txt)
 	pip3 install -r packages/python_packages.txt
@@ -14,12 +15,24 @@ if [[ ! ${CI} ]]; then
 
 fi
 
-scripts=(install_vim.sh install_fonts.sh install_zsh.sh)
+
+[[ $DOCKER = "yes" ]] && alias vim="nvim"
+
+scripts=(vim fonts zsh)
 for script in ${scripts[@]}; do
-	source install_scripts/packages/$script
+	source install_scripts/packages/"install_${script}.sh"
 done
 
 # don't want to install these inside CI
 if [[ ! ${CI} ]]; then
+
+	# use sed to replace ssh with https, because we don't have access to ssh keys inside docker container
+	if [[ $DOCKER = "yes" ]]; then
+		sed -i 's/git@github.com:/https:\/\/github.com\//' install_scripts/packages/install_suckless.sh
+
+		# create a place to put desktop session entry for dwm
+		sudo mkdir -p /usr/share/xsessions
+	fi
+
 	source install_scripts/packages/install_suckless.sh
 fi
