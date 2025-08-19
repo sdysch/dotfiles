87eb407cd6aa78e39d06223f57a3d98af110112b
Samuel Dezso Salling-Dysch <sdysch94@gmail.com>
2025-08-19 20:34:17 +0100

    Change install script CI check

--- install_scripts/install_xubuntu.sh
+++ install_scripts/install_xubuntu.sh
@@ -6,7 +6,6 @@ mkdir -p "${XDG_CACHE_HOME:-$HOME/.cache}"
 
 # don't want to install many packages for each CI run
 # they are also not needed for the docker container
-echo $DOCKER
 if [[ ! ${CI} ]] && [[ $DOCKER != "yes" ]] ; then
 	sudo apt-get install $(cat packages/packages_xubuntu.txt)
 
@@ -23,16 +22,13 @@ for script in ${scripts[@]}; do
 	source install_scripts/packages/"install_${script}.sh"
 done
 
-# don't want to install these inside CI
-if [[ ! ${CI} ]]; then
 
-	# use sed to replace ssh with https, because we don't have access to ssh keys inside docker container
-	if [[ $DOCKER == "yes" ]]; then
-		sed -i 's/git@github.com:/https:\/\/github.com\//' install_scripts/packages/install_suckless.sh
+# use sed to replace ssh with https, because we don't have access to ssh keys inside docker container
+if [[ $DOCKER == "yes" ]]; then
+	sed -i 's/git@github.com:/https:\/\/github.com\//' install_scripts/packages/install_suckless.sh
 
-		# create a place to put desktop session entry for dwm
-		sudo mkdir -p /usr/share/xsessions
-	fi
-
-	source install_scripts/packages/install_suckless.sh
+	# create a place to put desktop session entry for dwm
+	sudo mkdir -p /usr/share/xsessions
 fi
+
+source install_scripts/packages/install_suckless.sh
