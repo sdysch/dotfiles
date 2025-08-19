8ad794908c2e21dbf97316e14f49e8f4e9ec034d
Samuel Dezso Salling-Dysch <sdysch94@gmail.com>
2025-08-19 20:21:46 +0100

    Third attempt at CI update

--- install_scripts/packages/install_vim.sh
+++ install_scripts/packages/install_vim.sh
@@ -12,5 +12,5 @@ echo "Installing Vundle...."
 INSTALLDIR=${XDG_DATA_HOME:-$HOME/.local/share}/vim/bundle
 mkdir -p $INSTALLDIR
 git clone https://github.com/VundleVim/Vundle.vim.git $INSTALLDIR/Vundle.vim
-vim +PluginInstall +qall
+nvim +PluginInstall +qall
 echo "Done"
