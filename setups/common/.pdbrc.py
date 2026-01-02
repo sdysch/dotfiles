782c5d5110c9dad98a1173164c1ffeab488eb47a
Sam Dezso Salling-Dysch <samuel.salling-dysch@flyr.com>
2026-01-02 11:42:15 +0000

    pdb: Add config file

---
+++ setups/common/.pdbrc.py
@@ -0,0 +1,47 @@
+print('[INFO] -- Loading custom settings from ~/.pdbrc.py')
+from pygments.token import Token
+import os
+import pdb
+
+# vim keybindings in pager
+os.environ['PAGER'] = 'less -R'
+
+# === config ===
+class Config(pdb.DefaultConfig):
+    # shortcuts
+    editor = 'e'
+
+    # colours
+    setbgcolor = 'dark'
+    use_pygments = True
+    filename_color = pdb.Color.lightgray
+    use_terminal256formatter = True
+
+    # colours
+    setbgcolor = 'dark'
+    use_pygments = True
+
+    # prompt
+    prompt = '(Pdb++) → '
+
+    # auto context
+    context = 5
+
+    # sticky mode always on
+    sticky_by_default = True
+
+    def __init__(self):
+
+        try:
+            from pygments.formatters import terminal
+        except ImportError:
+            pass
+        else:
+            self.colorscheme = terminal.TERMINAL_COLORS.copy()
+            self.colorscheme.update({
+                terminal.Keyword:            (139, None),
+                terminal.Number:             (186, None),
+                terminal.String:             (108, None),
+                terminal.Name.Function:      (67, None),
+                terminal.Name.Namespace:     (110, None),
+            })
