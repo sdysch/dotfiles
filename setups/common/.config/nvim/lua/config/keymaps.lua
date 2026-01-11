ecd6ec4d56c65eea05f85b027955f2c21a13cc16
Samuel Dezso Salling-Dysch <sdysch94@gmail.com>
2026-01-11 18:38:58 +0000

    nvim: fzf search bindings/commands

--- setups/common/.config/nvim/lua/config/keymaps.lua
+++ setups/common/.config/nvim/lua/config/keymaps.lua
@@ -111,11 +111,15 @@ end, { buffer = true, desc = 'Insert common Python imports' })
 
 -- fzf
 local fzf = require('fzf-lua')
-vim.keymap.set('n', '<c-p>', fzf.git_files)
-vim.keymap.set('n', '<c-b>', fzf.buffers)
-vim.keymap.set('n', '<c-f>', fzf.live_grep)
-vim.keymap.set('n', '<leader>*', fzf.grep_cword)
-vim.keymap.set('n', '<leader>fr', fzf.oldfiles)
+vim.keymap.set('n', '<c-p>', fzf.git_files, { desc = 'fzf files in git' })
+vim.keymap.set('n', '<c-b>', fzf.buffers, { desc = 'fzf buffers' })
+vim.keymap.set('n', '<c-f>', fzf.live_grep, { desc = 'fzf live grep' })
+vim.keymap.set('n', '<leader>*', fzf.grep_cword, { desc = 'live grep on current word' })
+vim.keymap.set('n', '<leader>fr', fzf.oldfiles, { desc = 'Find recent files' })
+
+vim.keymap.set('n', '<leader>fk', ':FzfLua keymaps<CR>', { desc = 'FzfLua: Show keymaps' })
+vim.keymap.set('n', '<leader>fc', ':FzfLua commands<CR>', { desc = 'FzfLua: Show commands' })
+
 
 -- LSP keymaps
 local opts = { noremap = true, silent = true }
