3756909d19b04bc57effc26059e46885db88d3eb
Samuel Dezso Salling-Dysch <sdysch94@gmail.com>
2026-01-13 17:42:23 +0000

    nvim: vimwiki consolidation

--- setups/common/.config/nvim/lua/config/settings.lua
+++ setups/common/.config/nvim/lua/config/settings.lua
@@ -35,19 +35,3 @@ vim.opt.grepformat = '%f:%l:%c:%m'
 
 vim.api.nvim_create_user_command('Term', 'terminal', {})
 vim.api.nvim_create_user_command('Vterm', 'vertical terminal', {})
-
-
--- vimwiki
-vim.g.vimwiki_list = {{ path = os.getenv('VIMWIKI') }}
-vim.g.wiki_root = os.getenv('VIMWIKI')
-
-function WikiTodo()
-  local path = vim.fn.fnameescape(vim.g.wiki_root .. '/todo.wiki')
-  vim.cmd('e ' .. path)
-end
-
-function WikiNotes()
-  local path = vim.fn.fnameescape(vim.g.wiki_root .. '/notes.wiki')
-  vim.cmd('e ' .. path)
-end
-
