48c56e16501040b272631b9ac06ef9e22d73b280
Sam Dezso Salling-Dysch <samuel.salling-dysch@flyr.com>
2026-01-13 12:00:58 +0000

    nvim: copilot

--- setups/common/.config/nvim/lua/plugins/init.lua
+++ setups/common/.config/nvim/lua/plugins/init.lua
@@ -47,7 +47,47 @@ return {
   { 'junegunn/vim-peekaboo', event = 'VeryLazy' },
 
   -- copilot
-  -- { 'github/copilot.vim', lazy = false },
+  {
+    'zbirenbaum/copilot.lua',
+    dependencies = {
+      'copilotlsp-nvim/copilot-lsp',
+    },
+    cmd = { 'Copilot' },
+    keys = { { '<leader>ct', '<Cmd>Copilot toggle<CR>', mode = 'n', desc = 'Copilot toggle' } },
+    config = function()
+      require('copilot').setup({
+        panel = {
+          enabled = false,
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = '<C-l>',
+            -- next = '<C-]>',
+            -- prev = '<C-[>',
+            -- dismiss = '<C-]>',
+          },
+        },
+        filetypes = {
+          markdown = true,
+          python = true,
+          lua = true,
+          bash = true,
+          zsh = true,
+          sh = true,
+          gitcommit = false,
+          help = false,
+        },
+      })
+
+      vim.keymap.set('n', '<leader>ct', '<Cmd>Copilot toggle<CR>', { 
+        noremap = true, 
+        silent = true, 
+        desc = 'Toggle Copilot' 
+      })
+    end,
+  },
 
   -- colorscheme
   { 'gbprod/nord.nvim', lazy = false, priority = 1000 },
