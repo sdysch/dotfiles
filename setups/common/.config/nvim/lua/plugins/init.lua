f8da3dad9d50c4db471e28c2016abcb4fe90d7f4
Samuel Dezso Salling-Dysch <sdysch94@gmail.com>
2026-01-13 17:58:37 +0000

    nvim: consolidate fugitive

--- setups/common/.config/nvim/lua/plugins/init.lua
+++ setups/common/.config/nvim/lua/plugins/init.lua
@@ -2,7 +2,17 @@
 return {
 
   -- git
-  { 'tpope/vim-fugitive', cmd = { 'Git', 'G', 'Gstatus', 'Gdiffsplit'} },
+  {
+	  'tpope/vim-fugitive',
+	  cmd = { 'Git', 'G', 'Gstatus', 'Gdiffsplit'},
+	  keys = {
+		  { '<leader>gs', '<cmd>Git status<CR>', desc = 'Git status', noremap = true, silent = true },
+		  { '<leader>gc', '<cmd>Git commit<CR>', desc = 'Git commit', noremap = true, silent = true },
+		  { '<leader>gd', '<cmd>Gdiffsplit<CR>', desc = 'Git diffsplit', noremap = true, silent = true },
+		  { '<leader>gt', '<cmd>G difftool<CR>', desc = 'Git difftool', noremap = true, silent = true },
+	  }
+
+  },
 
   -- commenting
   { 'preservim/nerdcommenter', keys = { 'gc', 'gcc' } },
