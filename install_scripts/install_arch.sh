ff384831788db8732065ca4b124790d8cfc0ddde
Samuel Dezso Salling-Dysch <sdysch94@gmail.com>
2026-03-05 10:28:11 +0000

    Skip package install inside docker images

--- install_scripts/install_arch.sh
+++ install_scripts/install_arch.sh
@@ -6,6 +6,10 @@ is_ci() {
     [[ -n "${CI:-}" ]]
 }
 
+is_docker() {
+    grep -qE '/docker|/lxc' /proc/1/cgroup 2>/dev/null || [ -f /.dockerenv ]
+}
+
 log() {
     printf '\n\033[1;34m==> %s\033[0m\n' "$1"
 }
@@ -86,7 +90,7 @@ _install_zsh() {
 
 _install_packages() {
 	log 'Installing packages, pacman & AUR'
-	if ! is_ci; then
+	if ! is_ci && ! is_docker; then
 		log 'Installing pacman packages'
 		# shellcheck disable=SC2024
 		sudo pacman -S --needed --noconfirm - < packages/packages_arch.txt
