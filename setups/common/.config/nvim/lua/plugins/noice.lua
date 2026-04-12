4e89106f222ee990cc768e70f6a609ccb2b7985b
Sam Salling-Dysch <48129845+sdysch@users.noreply.github.com>
2026-04-12 09:41:56 +0100

    Dev/nvim plugin clean (#25)

--- setups/common/.config/nvim/lua/plugins/noice.lua
+++ setups/common/.config/nvim/lua/plugins/noice.lua
@@ -1,44 +1,44 @@
 return {
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      {
-        "rcarriga/nvim-notify",
-        config = function()
-          vim.notify = require("notify")
+  -- {
+  --   "folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     {
+  --       "rcarriga/nvim-notify",
+  --       config = function()
+  --         vim.notify = require("notify")
 
-          require("notify").setup({
-            stages = "fade",
-            timeout = 3000,
-            background_colour = "#000000",
-          })
-        end,
-      },
-    },
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-        hover = {
-          enabled = false,
-        },
-        signature = {
-          enabled = false,
-        },
-      },
+  --         require("notify").setup({
+  --           stages = "fade",
+  --           timeout = 3000,
+  --           background_colour = "#000000",
+  --         })
+  --       end,
+  --     },
+  --   },
+  --   opts = {
+  --     lsp = {
+  --       override = {
+  --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+  --         ["vim.lsp.util.stylize_markdown"] = true,
+  --         ["cmp.entry.get_documentation"] = true,
+  --       },
+  --       hover = {
+  --         enabled = false,
+  --       },
+  --       signature = {
+  --         enabled = false,
+  --       },
+  --     },
 
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = false,
-        lsp_doc_border = true,
-      },
-    },
-  },
+  --     presets = {
+  --       bottom_search = true,
+  --       command_palette = true,
+  --       long_message_to_split = true,
+  --       inc_rename = false,
+  --       lsp_doc_border = true,
+  --     },
+  --   },
+  -- },
 }
--- setups/common/.config/nvim/lua/plugins/nvim_neotest.lua
+++ setups/common/.config/nvim/lua/plugins/nvim_neotest.lua
@@ -1,73 +1,73 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-neotest/neotest-python",
-  },
-  keys = {
-    {
-      "<leader>tr",
-      function()
-        require("neotest").run.run()
-      end,
-      desc = "Neotest: run nearest test",
-    },
-    {
-      "<leader>tf",
-      function()
-        require("neotest").run.run(vim.fn.expand("%"))
-      end,
-      desc = "Neotest: run tests in file",
-    },
-    {
-      "<leader>ta",
-      function()
-        require("neotest").run.run({ suite = true })
-      end,
-      desc = "Neotest: run all tests",
-    },
-    {
-      "<leader>ts",
-      function()
-        require("neotest").summary.toggle()
-      end,
-      desc = "Neotest: toggle summary",
-    },
-    {
-      "<leader>tl",
-      function()
-        require("neotest").run.run_last()
-      end,
-      desc = "Neotest: run last test",
-    },
-  },
-  config = function()
-    require("neotest").setup({
-      adapters = {
-        require("neotest-python")({
-          dap = { justMyCode = false },
-          args = { "--log-level", "DEBUG" },
-          runner = "pytest",
-          python = ".venv/bin/python",
-          pytest_discover_instances = false,
-        }),
-      },
-      icons = {
-        passed = "✔",
-        running = "⏱",
-        failed = "✖",
-        skipped = "➟",
-        unknown = "?",
-      },
-      quickfix = {
-        enabled = false,
-      },
-    })
+  -- "nvim-neotest/neotest",
+  -- dependencies = {
+  --   "nvim-neotest/nvim-nio",
+  --   "nvim-lua/plenary.nvim",
+  --   "antoinemadec/FixCursorHold.nvim",
+  --   "nvim-treesitter/nvim-treesitter",
+  --   "nvim-neotest/neotest-python",
+  -- },
+  -- keys = {
+  --   {
+  --     "<leader>tr",
+  --     function()
+  --       require("neotest").run.run()
+  --     end,
+  --     desc = "Neotest: run nearest test",
+  --   },
+  --   {
+  --     "<leader>tf",
+  --     function()
+  --       require("neotest").run.run(vim.fn.expand("%"))
+  --     end,
+  --     desc = "Neotest: run tests in file",
+  --   },
+  --   {
+  --     "<leader>ta",
+  --     function()
+  --       require("neotest").run.run({ suite = true })
+  --     end,
+  --     desc = "Neotest: run all tests",
+  --   },
+  --   {
+  --     "<leader>ts",
+  --     function()
+  --       require("neotest").summary.toggle()
+  --     end,
+  --     desc = "Neotest: toggle summary",
+  --   },
+  --   {
+  --     "<leader>tl",
+  --     function()
+  --       require("neotest").run.run_last()
+  --     end,
+  --     desc = "Neotest: run last test",
+  --   },
+  -- },
+  -- config = function()
+  --   require("neotest").setup({
+  --     adapters = {
+  --       require("neotest-python")({
+  --         dap = { justMyCode = false },
+  --         args = { "--log-level", "DEBUG" },
+  --         runner = "pytest",
+  --         python = ".venv/bin/python",
+  --         pytest_discover_instances = false,
+  --       }),
+  --     },
+  --     icons = {
+  --       passed = "✔",
+  --       running = "⏱",
+  --       failed = "✖",
+  --       skipped = "➟",
+  --       unknown = "?",
+  --     },
+  --     quickfix = {
+  --       enabled = false,
+  --     },
+  --   })
 
-    -- Make sure the sign column is always visible
-    vim.o.signcolumn = "yes"
-  end,
+  --   -- Make sure the sign column is always visible
+  --   vim.o.signcolumn = "yes"
+  -- end,
 }
