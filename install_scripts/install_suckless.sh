00fef9257c0e209725fa193446850c7dcdf6171c
Samuel Dezso Salling-Dysch <sdysch94@gmail.com>
2026-06-24 10:21:39 +0100

    Install suckless into Projects

--- install_scripts/install_suckless.sh
+++ install_scripts/install_suckless.sh
@@ -2,7 +2,7 @@
 function install() {
 	echo "Installing $1 ....."
 	sleep 1
-	INSTALLDIR="$HOME/Documents/repos/$1"
+	INSTALLDIR="$HOME/Projects/$1"
 	mkdir -p "$INSTALLDIR"
 	git clone https://github.com/sdysch/"$1".git "$INSTALLDIR"
 	pushd "$INSTALLDIR" || exit 1
